35f7ad3eb81cc4f81957ab46dbc2305aab9da487
Ahmed Hassan <ahmed@ahmedhassan.me>
2014-12-17 09:43:08 +0000

    Added real converter

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -1,14 +1,9 @@
-//
-//  ViewController.swift
-//  AgeOfLaika
-//
-//  Created by Ahmed Hassan on 16/12/2014.
-//  Copyright (c) 2014 Ahmed Hassan. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var dogYearsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +15,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanYears = Double((humanYearsTextField.text as NSString).doubleValue)
+        let conversionConstant = 7.0
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(humanYears * conversionConstant)" + " human years"
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+    }
+
+    @IBAction func convertToDogYearsAccurateButtonPressed(sender: UIButton) {
+        let humanYears = Double((humanYearsTextField.text as NSString).doubleValue)
+        dogYearsLabel.hidden = false
+        if humanYears <= 2.0 {
+            dogYearsLabel.text = "\(humanYears * 10.5)" + " human years"
+        }
+        else {
+            dogYearsLabel.text = "\((2 * 10.5) + ((humanYears - 2) * 4))" + " human years"
+        }
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
